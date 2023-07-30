5adde2490c52be036b93373980fdc2ff5ccdd1b4
b.Mashrapov <b_mashrapov@mail.ru>
2023-07-30 15:56:58 +0200

    1. Listener, keyBoard classes have been changed. 2.Processing of primary commands by the bot is implemented.

--- src/main/java/pro/sky/animalshelter/keyBoard/InlineKeyboardMarkupHelper.java
+++ src/main/java/pro/sky/animalshelter/keyBoard/InlineKeyboardMarkupHelper.java
@@ -8,17 +8,15 @@ import java.util.List;
 
 public class InlineKeyboardMarkupHelper {
 
-    public static InlineKeyboardMarkup createInlineKeyboard(String text, String[] buttonLabels) {
+    public static InlineKeyboardMarkup createInlineKeyboard() {
         List<InlineKeyboardButton> rowInline = new ArrayList<>();
-
-        for (String label : buttonLabels) {
-            InlineKeyboardButton button = new InlineKeyboardButton(label).callbackData(label);
-            rowInline.add(button);
-        }
-
+        rowInline.add(new InlineKeyboardButton("Приют кошек\uD83D\uDC31").callbackData("Приют кошек\uD83D\uDC31"));
+        rowInline.add(new InlineKeyboardButton("Приют собак\uD83D\uDC36").callbackData("Приют собак\uD83D\uDC36"));
         InlineKeyboardButton[] buttonsArray = rowInline.toArray(new InlineKeyboardButton[0]);
         return new InlineKeyboardMarkup(buttonsArray);
+
     }
+
     public static InlineKeyboardMarkup createMainMenuInlineKeyboard() {
         List<InlineKeyboardButton> rowInline = new ArrayList<>();
 
@@ -33,20 +31,22 @@ public class InlineKeyboardMarkupHelper {
 
         // Кнопка "Позвать волонтера"
         rowInline.add(new InlineKeyboardButton("4").callbackData("CallVolunteer"));
+        // Кнопка "⬅ Назад"
+        rowInline.add(new InlineKeyboardButton("⬅ Назад").callbackData("BackShelterMenu"));
 
         InlineKeyboardButton[] buttonsArray = rowInline.toArray(new InlineKeyboardButton[0]);
         return new InlineKeyboardMarkup(buttonsArray);
     }
+
     public static InlineKeyboardMarkup createShelterInfoInlineKeyboard() {
         List<InlineKeyboardButton> rowInline = new ArrayList<>();
-
-        // Add buttons for shelter information options
-        rowInline.add(new InlineKeyboardButton("О приюте").callbackData("ShelterInfoAbout"));
-        rowInline.add(new InlineKeyboardButton("Расписание и адрес").callbackData("ShelterInfoAddress"));
-        rowInline.add(new InlineKeyboardButton("Контакты охраны").callbackData("ShelterInfoSecurityContacts"));
-        rowInline.add(new InlineKeyboardButton("Техника безопасности").callbackData("ShelterInfoSafetyTips"));
-        rowInline.add(new InlineKeyboardButton("Оставить контакты").callbackData("ShelterInfoLeaveContacts"));
-        rowInline.add(new InlineKeyboardButton("Позвать волонтера").callbackData("ShelterInfoCallVolunteer"));
+        rowInline.add(new InlineKeyboardButton("1").callbackData("ShelterInfoAbout"));
+        rowInline.add(new InlineKeyboardButton("2").callbackData("ShelterInfoAddress"));
+        rowInline.add(new InlineKeyboardButton("3").callbackData("ShelterInfoSecurityContacts"));
+        rowInline.add(new InlineKeyboardButton("4").callbackData("ShelterInfoSafetyTips"));
+        rowInline.add(new InlineKeyboardButton("5").callbackData("ShelterInfoLeaveContacts"));
+        rowInline.add(new InlineKeyboardButton("6").callbackData("ShelterInfoCallVolunteer"));
+        rowInline.add(new InlineKeyboardButton("⬅").callbackData("BackMainMenu"));
 
         InlineKeyboardButton[] buttonsArray = rowInline.toArray(new InlineKeyboardButton[0]);
         return new InlineKeyboardMarkup(buttonsArray);
@@ -55,20 +55,18 @@ public class InlineKeyboardMarkupHelper {
     // Method to create inline keyboard for "Adoption rules" options
     public static InlineKeyboardMarkup createAdoptionRulesInlineKeyboard() {
         List<InlineKeyboardButton> rowInline = new ArrayList<>();
+        rowInline.add(new InlineKeyboardButton("1").callbackData("AdoptionRulesIntroduction"));
+        rowInline.add(new InlineKeyboardButton("2").callbackData("AdoptionRulesDocuments"));
+        rowInline.add(new InlineKeyboardButton("3").callbackData("AdoptionRulesTransport"));
+        rowInline.add(new InlineKeyboardButton("4").callbackData("AdoptionRulesHouseSetupPuppyKitten"));
+        rowInline.add(new InlineKeyboardButton("5").callbackData("AdoptionRulesHouseSetupAdult"));
+        rowInline.add(new InlineKeyboardButton("6").callbackData("AdoptionRulesHouseSetupSpecialNeeds"));
+        rowInline.add(new InlineKeyboardButton("7").callbackData("AdoptionRulesRejectionReasons"));
+        rowInline.add(new InlineKeyboardButton("8").callbackData("AdoptionRulesLeaveContacts"));
+        rowInline.add(new InlineKeyboardButton("9").callbackData("AdoptionRulesCallVolunteer"));
+        rowInline.add(new InlineKeyboardButton("⬅").callbackData("BackMainMenu"));
 
-        // Add buttons for adoption rules options
-        rowInline.add(new InlineKeyboardButton("Приветствие").callbackData("AdoptionRulesGreeting"));
-        rowInline.add(new InlineKeyboardButton("Правила знакомства").callbackData("AdoptionRulesIntroduction"));
-        rowInline.add(new InlineKeyboardButton("Список документов").callbackData("AdoptionRulesDocuments"));
-        rowInline.add(new InlineKeyboardButton("Транспортировка животного").callbackData("AdoptionRulesTransport"));
-        rowInline.add(new InlineKeyboardButton("Обустройство дома (щенок/котенок)").callbackData("AdoptionRulesHouseSetupPuppyKitten"));
-        rowInline.add(new InlineKeyboardButton("Обустройство дома (взрослое животное)").callbackData("AdoptionRulesHouseSetupAdult"));
-        rowInline.add(new InlineKeyboardButton("Обустройство дома (с ограничениями)").callbackData("AdoptionRulesHouseSetupSpecialNeeds"));
-        // Skip the dog-specific options for a cat shelter
-
-        rowInline.add(new InlineKeyboardButton("Причины отказа").callbackData("AdoptionRulesRejectionReasons"));
-        rowInline.add(new InlineKeyboardButton("Оставить контакты").callbackData("AdoptionRulesLeaveContacts"));
-        rowInline.add(new InlineKeyboardButton("Позвать волонтера").callbackData("AdoptionRulesCallVolunteer"));
+        //тут придумать логику работы метода и разделить информацию для кошек и собак. После этого добавить кнопки по советам а также кантактам кинологов для приюта собак
 
         InlineKeyboardButton[] buttonsArray = rowInline.toArray(new InlineKeyboardButton[0]);
         return new InlineKeyboardMarkup(buttonsArray);
--- src/main/java/pro/sky/animalshelter/listener/AnimalShelterUpdatesListener.java
+++ src/main/java/pro/sky/animalshelter/listener/AnimalShelterUpdatesListener.java
@@ -53,22 +53,19 @@ public class AnimalShelterUpdatesListener implements UpdatesListener {
 
                         if ("/start".equals(text)) {
                             SendMessage response = new SendMessage(chatId,
-                                    "Добро пожаловать в приют для животных! Выберите приют для каких животных вас интересует:");
+                                    "Добро пожаловать в приют для животных города Астаны! Выберите приют для каких животных вас интересует:");
                             InlineKeyboardMarkup keyboardMarkup = InlineKeyboardMarkupHelper.createInlineKeyboard(
-                                    "Выберите приют:",
-                                    new String[]{"Приют кошек\uD83D\uDC31", "Приют собак\uD83D\uDC36"}
                             );
                             response.replyMarkup(keyboardMarkup);
                             SendResponse sendResponse = animalShelterBot.execute(response);
                             logger.info("Message sent status: {}", sendResponse.isOk());
                         } else {
                             InlineKeyboardMarkup keyboardMarkup = InlineKeyboardMarkupHelper.createInlineKeyboard(
-                                    "Выберите приют:",
-                                    new String[]{"Приют кошек\uD83D\uDC31", "Приют собак\uD83D\uDC36"}
                             );
                             sendMessage(chatId, "Некорректное сообщение.", keyboardMarkup);
                         }
                     });
+
             updates.stream()
                     .filter(update -> update.callbackQuery() != null)
                     .forEach(update -> {
@@ -79,30 +76,73 @@ public class AnimalShelterUpdatesListener implements UpdatesListener {
 
                         // Обработка callback-запросов от нажатых кнопок
                         if ("Приют кошек\uD83D\uDC31".equals(data) || "Приют собак\uD83D\uDC36".equals(data)) {
-                            SendMessage response = new SendMessage(chatId,  data + " приветствует Вас! Спасибо за выбор! Выберите, что вас интересует:\n" +
+                            SendMessage response = new SendMessage(chatId, data + " приветствует Вас! Спасибо за выбор! Выберите, что вас интересует:\n" +
                                     "1. Информация о приюте\n" +
                                     "2. Как забрать животное\n" +
                                     "3. Отправить отчет о животном\n" +
-                                    "4. Позвать волонтера ");
+                                    "4. Позвать волонтера\n" +
+                                    "⬅ Назад");
                             InlineKeyboardMarkup menuKeyboard = InlineKeyboardMarkupHelper.createMainMenuInlineKeyboard();
                             response.replyMarkup(menuKeyboard);
-                            sendMessage(chatId, String.valueOf(response), menuKeyboard);
                             SendResponse sendResponse = animalShelterBot.execute(response);
                             logger.info("Message sent status: {}", sendResponse.isOk());
                         } else if ("Info".equals(data)) {
-                            String infoMessage = "Информация о приюте: ...";
+                            SendMessage responseInfo = new SendMessage(chatId, "Выберите что вы хотите узнать о прюте:\n" +
+                                    "1. О приюте\n" +
+                                    "2. Расписание и адрес\n" +
+                                    "3. Контакты охраны\n" +
+                                    "4. Техника безопасности\n" +
+                                    "5. Оставить контакты\n" +
+                                    "6. Позвать волонтера\n" +
+                                    "⬅ Назад");
                             InlineKeyboardMarkup shelterInfoKeyboard = InlineKeyboardMarkupHelper.createShelterInfoInlineKeyboard();
-                            sendMessage(chatId, infoMessage, shelterInfoKeyboard);
+                            responseInfo.replyMarkup(shelterInfoKeyboard);
+                            SendResponse sendResponseInfo = animalShelterBot.execute(responseInfo);
+                            logger.info("Message sent status: {}", sendResponseInfo.isOk());
                         } else if ("AdoptionRules".equals(data)) {
-                            String adoptionRulesMessage = "Как забрать животное: ...";
+                            SendMessage responseAdoptionRules = new SendMessage(chatId, "Мы рады, что вы заинтересованы помочь нашим пушистым друзьям. Но перед этим ознакомтесь с правилами:\n" +
+                                    "1. Правила знакомства с животными \n" +
+                                    "2. Список необходимых документов для усыновления\n" +
+                                    "3. Транспортировка животного\n" +
+                                    "4. Обустройство дома для самых маленьких\n" +
+                                    "5. Обустройство дома для взрослого животного\n" +
+                                    "6. Обустройство дома для животных с ограниченными возможностями\n" +
+                                    "7. Причины отказа\n" +
+                                    "8. Оставьте нам свои контакты для связи\n" +
+                                    "9. Позвать волонтера\n" +
+                                    "⬅ Назад"
+
+                                    //В теллеграм ограничение по выводимым кнопкам. Максимально 8шт. Нужно будет один пункт сократить.
+                            );
                             InlineKeyboardMarkup adoptionRulesKeyboard = InlineKeyboardMarkupHelper.createAdoptionRulesInlineKeyboard();
-                            sendMessage(chatId, adoptionRulesMessage, adoptionRulesKeyboard);
+                            responseAdoptionRules.replyMarkup(adoptionRulesKeyboard);
+                            SendResponse sendResponseAdoptionRules = animalShelterBot.execute(responseAdoptionRules);
+                            logger.info("Message sent status: {}", sendResponseAdoptionRules.isOk());
                         } else if ("SendReport".equals(data)) {
-                            String reportMessage = "Пожалуйста, пришлите:\n- *Фото животного.*\n- *Рацион животного.*\n- *Общее самочувствие и привыкание к новому месту.*\n- *Изменение в поведении: отказ от старых привычек, приобретение новых.*";
+                            String reportMessage = "Пожалуйста, пришлите:\n- Фото животного.\n- Рацион животного.\n- Общее самочувствие и привыкание к новому месту.\n- Изменение в поведении: отказ от старых привычек, приобретение новых.";
                             sendMessage(chatId, reportMessage, null);
+                            //обработать метод принятия сообщений от пользователя и внесение в БД
                         } else if ("CallVolunteer".equals(data)) {
                             String volunteerMessage = "Волонтер в пути! Ожидайте!";
                             sendMessage(chatId, volunteerMessage, null);
+                            //придумать логику связи пользователя с волонтером
+                        }else if ("BackShelterMenu".equals(data)) { // Обработка возврата в главное меню
+                            SendMessage response = new SendMessage(chatId, "Вы вернулись в главное меню. Выберите приют для каких животных вас интересует: ");
+                            InlineKeyboardMarkup menuKeyboard = InlineKeyboardMarkupHelper.createInlineKeyboard();
+                            response.replyMarkup(menuKeyboard);
+                            SendResponse sendResponse = animalShelterBot.execute(response);
+                            logger.info("Message sent status: {}", sendResponse.isOk());
+                        } else if ("BackMainMenu".equals(data)) { // Обработка возврата в меню "Информация о приюте"
+                            SendMessage responseInfo = new SendMessage(chatId, "Вы вернулись в главное меню приюта. Выберите что вы хотите узнать о прюте:\n" +
+                                    "1. Информация о приюте\n" +
+                                    "2. Как забрать животное\n" +
+                                    "3. Отправить отчет о животном\n" +
+                                    "4. Позвать волонтера\n" +
+                                    "⬅ Назад");
+                            InlineKeyboardMarkup shelterInfoKeyboard = InlineKeyboardMarkupHelper.createMainMenuInlineKeyboard();
+                            responseInfo.replyMarkup(shelterInfoKeyboard);
+                            SendResponse sendResponseInfo = animalShelterBot.execute(responseInfo);
+                            logger.info("Message sent status: {}", sendResponseInfo.isOk());
                         }
                     });
         } catch (Exception e) {
@@ -110,52 +150,53 @@ public class AnimalShelterUpdatesListener implements UpdatesListener {
         }
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
     }
-
-    private void handleCallbackQuery(CallbackQuery callbackQuery) {
-        logger.info("Handles callback query: {}", callbackQuery);
-        Long chatId = callbackQuery.message().chat().id();
-        String data = callbackQuery.data();
-
-        if (data.startsWith("ShelterInfo")) {
-            // Handle shelter information options
-            String shelterInfoOption = data.replace("ShelterInfo", "");
-            switch (shelterInfoOption) {
-                case "About":
-                    // Provide information about the shelter
-                    sendMessage(chatId, "О приюте: ...", null);
-                    break;
-                case "Address":
-                    // Provide schedule and address information
-                    sendMessage(chatId, "Расписание и адрес: ...", null);
-                    break;
-                case "SecurityContacts":
-                    // Provide security contacts for car passes
-                    sendMessage(chatId, "Контакты охраны: ...", null);
-                    break;
-                case "SafetyTips":
-                    // Provide general safety tips within the shelter
-                    sendMessage(chatId, "Техника безопасности: ...", null);
-                    break;
-                case "LeaveContacts":
-                    // Allow the user to leave their contacts for communication
-                    sendMessage(chatId, "Оставьте ваши контакты...", null);
-                    break;
-                case "CallVolunteer":
-                    // Initiate a live chat with a volunteer
-                    // Handle this functionality as needed based on your requirements
-                    break;
-                default:
-                    // Handle unknown shelter information option
-                    break;
-            }
-        } else if (data.startsWith("AdoptionRules")) {
-            // Handle adoption rules options
-            String adoptionRulesOption = data.replace("AdoptionRules", "");
-            switch (adoptionRulesOption) {
-                // Implement the logic for adoption rules options similarly to shelter information
-                // options above using switch-case statements.
-            }
-        }
-        // Handle other callback queries here if needed
-    }
+// ----------- После обработки логики приложения выше написанный код будет обернут в switch на основе кода написанного ниже.
+// ----------- Просьба пока не удалять ниде написанный код:))
+//    private void handleCallbackQuery(CallbackQuery callbackQuery) {
+//        logger.info("Handles callback query: {}", callbackQuery);
+//        Long chatId = callbackQuery.message().chat().id();
+//        String data = callbackQuery.data();
+//
+//        if (data.startsWith("ShelterInfo")) {
+//            // Handle shelter information options
+//            String shelterInfoOption = data.replace("ShelterInfo", "");
+//            switch (shelterInfoOption) {
+//                case "About":
+//                    // Provide information about the shelter
+//                    sendMessage(chatId, "О приюте: ...", null);
+//                    break;
+//                case "Address":
+//                    // Provide schedule and address information
+//                    sendMessage(chatId, "Расписание и адрес: ...", null);
+//                    break;
+//                case "SecurityContacts":
+//                    // Provide security contacts for car passes
+//                    sendMessage(chatId, "Контакты охраны: ...", null);
+//                    break;
+//                case "SafetyTips":
+//                    // Provide general safety tips within the shelter
+//                    sendMessage(chatId, "Техника безопасности: ...", null);
+//                    break;
+//                case "LeaveContacts":
+//                    // Allow the user to leave their contacts for communication
+//                    sendMessage(chatId, "Оставьте ваши контакты...", null);
+//                    break;
+//                case "CallVolunteer":
+//                    // Initiate a live chat with a volunteer
+//                    // Handle this functionality as needed based on your requirements
+//                    break;
+//                default:
+//                    // Handle unknown shelter information option
+//                    break;
+//            }
+//        } else if (data.startsWith("AdoptionRules")) {
+//            // Handle adoption rules options
+//            String adoptionRulesOption = data.replace("AdoptionRules", "");
+//            switch (adoptionRulesOption) {
+//                // Implement the logic for adoption rules options similarly to shelter information
+//                // options above using switch-case statements.
+//            }
+//        }
+//        // Handle other callback queries here if needed
+//    }
 }
